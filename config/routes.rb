747a8edbcf46fdbfb3509715c13e888592d32e38
littlephillips <phillipkinuthia72@gmail.com>
2022-12-03 18:19:11 +0300

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  # resources :heroes, only: [:index, :show]
-  # resources :hero_powers, only: [:create]
-  # resources :powers, only: [:index, :show, :update]
+  resources :heroes, only: [:index, :show]
+  resources :hero_powers, only: [:create]
+  resources :powers, only: [:index, :show, :update]
 
 end
